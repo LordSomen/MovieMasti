ace52953a9335e88a921ced0c1cfce45d8b9d25c
LordSomen <soumyajit637@gmail.com>
2017-09-28 12:47:56 +0530

    extracts more json data

--- app/src/main/java/com/example/android/moviemasti/DataManipulation/JsonDataParsing.java
+++ app/src/main/java/com/example/android/moviemasti/DataManipulation/JsonDataParsing.java
@@ -12,17 +12,21 @@ import java.util.ArrayList;
 
 public class JsonDataParsing {
 
-    public static ArrayList<String> getDataForPopularity(String jsonPopularMovieData) throws JSONException{
-        ArrayList<String> imageDataArray = new ArrayList<>();
+    public static ArrayList<MovieData> getDataForPopularity(String jsonPopularMovieData) throws JSONException{
+        ArrayList<MovieData> imageDataArray = new ArrayList<>();
         JSONObject popularMovieJson = new JSONObject(jsonPopularMovieData);
         JSONArray movieList = popularMovieJson.getJSONArray("results");
         for(int i=0 ; i<movieList.length() ; i++){
             JSONObject movieListItem = movieList.getJSONObject(i);
-            String image_loc = movieListItem.getString("poster_path");
-            imageDataArray.add(image_loc);
+            String moviePosterPath = movieListItem.getString("poster_path");
+            int movieId = movieListItem.getInt("id");
+            double movieVoteAverage = movieListItem.getDouble("vote_average");
+            double moviePopularity = movieListItem.getDouble("popularity");
+            String movieTitle = movieListItem.getString("title");
+            String movieBackDropPath = movieListItem.getString("backdrop_path");
+            imageDataArray.add(new MovieData(movieId,movieVoteAverage,movieTitle,moviePosterPath,moviePopularity,movieBackDropPath));
         }
         return imageDataArray;
     }
-
 
 }
