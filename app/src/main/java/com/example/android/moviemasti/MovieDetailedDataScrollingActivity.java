eea8c0b656c34f6d3cbad5fdd31cdfe45df236d4
LordSomen <soumyajit637@gmail.com>
2017-09-28 12:53:37 +0530

    added the main class for showing detatils about movie

--- app/src/main/java/com/example/android/moviemasti/MovieDetailedDataScrollingActivity.java
+++ app/src/main/java/com/example/android/moviemasti/MovieDetailedDataScrollingActivity.java
@@ -1,13 +1,21 @@
 package com.example.android.moviemasti;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.moviemasti.DataManipulation.MovieData;
 
 public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
+
+    private TextView mContentTextview;
+    private String intentText;
+    private AppBarLayout mAppBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,7 @@ public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_detailed_data_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mAppBarLayout = (AppBarLayout)findViewById(R.id.app_bar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +32,29 @@ public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mContentTextview = (TextView) findViewById(R.id.content_text);
+
+        Bundle intentData = getIntent().getExtras();
+        MovieData movieData = (MovieData)intentData.getParcelable("movieIntentData");
+        if(movieData!=null){
+            Long movieId = movieData.getMovieId();
+            String movieTitle = movieData.getMovieTitle();
+            String moviePosterPath = movieData.getMoviePosterPath();
+            String movieBackdropPath = movieData.getMovieBackdropPath();
+            mContentTextview.setText(movieTitle+"\n\n");
+            mContentTextview.append(moviePosterPath+"\n\n");
+            mContentTextview.append(movieBackdropPath+"\n\n");
+
+        }
+
     }
+
+
+  /*  public void loadingImage() {
+        if(intentText!=null) {
+            String imgUrl = "https://image.tmdb.org/t/p/w500" + intentText;
+            Glide.with(this).load(imgUrl).into();
+        }
+    }*/
 }
