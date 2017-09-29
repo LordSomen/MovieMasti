6721fc4ce3c5204cc149862809f9498213de355a
LordSomen <soumyajit637@gmail.com>
2017-09-29 10:55:40 +0530

    added sorting function

--- app/src/main/java/com/example/android/moviemasti/MenusManipulation/SortingMovieData.java
+++ app/src/main/java/com/example/android/moviemasti/MenusManipulation/SortingMovieData.java
@@ -1,8 +1,28 @@
 package com.example.android.moviemasti.MenusManipulation;
 
+import android.util.Log;
+
+import com.example.android.moviemasti.DataManipulation.MovieData;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by soumyajit on 28/9/17.
  */
 
 public class SortingMovieData {
+
+        public static ArrayList<MovieData> sortAccordingToPopularity(ArrayList<MovieData> movieDataArrayList){
+            Collections.sort(movieDataArrayList,MovieData.popularity);
+            for(MovieData b : movieDataArrayList)
+                Log.i("SortingMovieData.class","output:"+b.getMoviePopularity()+"\n\n");
+            return movieDataArrayList;
+        }
+    public static ArrayList<MovieData> sortAccordingToRating(ArrayList<MovieData> movieDataArrayList){
+        Collections.sort(movieDataArrayList,MovieData.rating);
+        for(MovieData b : movieDataArrayList)
+            Log.i("SortingMovieData.class","output:"+b.getMoviePopularity()+"\n\n");
+        return movieDataArrayList;
+    }
 }
