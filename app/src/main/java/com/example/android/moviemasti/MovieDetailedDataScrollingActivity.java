f0d6eb3a67ea2a8bc2c8844b9c6a82b371699cf3
LordSomen <soumyajit637@gmail.com>
2017-10-05 13:50:13 +0530

    ui polishing in movie detail activity

--- app/src/main/java/com/example/android/moviemasti/MovieDetailedDataScrollingActivity.java
+++ app/src/main/java/com/example/android/moviemasti/MovieDetailedDataScrollingActivity.java
@@ -5,11 +5,9 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 
 import com.example.android.moviemasti.DataManipulation.MovieData;
 import com.squareup.picasso.Picasso;
@@ -19,13 +17,15 @@ public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
     private TextView mContentTextView;
     private AppBarLayout mAppBarLayout;
     private ImageView mBackDropImageView;
+    private ImageView mPosterImageView;
+    private TextView mMovieTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detailed_data_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       //setSupportActionBar(toolbar);
         mAppBarLayout = (AppBarLayout)findViewById(R.id.app_bar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,9 +35,10 @@ public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
+        mMovieTitle = (TextView)findViewById(R.id.movie_title);
         mContentTextView = (TextView) findViewById(R.id.content_text);
         mBackDropImageView = (ImageView)findViewById(R.id.movie_backdrop_image);
+        mPosterImageView = (ImageView)findViewById(R.id.movie_poster_image);
         Bundle intentData = getIntent().getExtras();
         MovieData movieData = intentData.getParcelable("movieIntentData");
         if(movieData!=null){
@@ -46,18 +47,26 @@ public class MovieDetailedDataScrollingActivity extends AppCompatActivity {
             String movieDescription = movieData.getMovieDescription();
             String moviePosterPath = movieData.getMoviePosterPath();
             String movieBackdropPath = movieData.getMovieBackdropPath();
-            getSupportActionBar().setTitle(movieTitle);
+            mMovieTitle.setText(movieTitle);
+          //  getSupportActionBar().setWindowTitle()(movieTitle);
             mContentTextView.setText(movieDescription);
-            loadingImage(movieBackdropPath);
+            loadingMovieBackDropImage(movieBackdropPath);
+            loadingMoviePosterImage(moviePosterPath);
         }
 
     }
 
-
-   public void loadingImage(String imgUrlPartBackDrop) {
+    public void loadingMovieBackDropImage(String imgUrlPartBackDrop) {
         if(imgUrlPartBackDrop!=null) {
             String imgUrl = "https://image.tmdb.org/t/p/w500" + imgUrlPartBackDrop;
             Picasso.with(this).load(imgUrl).into(mBackDropImageView);
+        }
+    }
+
+    public void loadingMoviePosterImage(String imgUrlPartPoster){
+        if(imgUrlPartPoster!=null) {
+            String imgUrl = "https://image.tmdb.org/t/p/w500" + imgUrlPartPoster;
+            Picasso.with(this).load(imgUrl).into(mPosterImageView);
         }
     }
 }
